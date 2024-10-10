6d820ada3362954c7b0859af81a25d63bb1eaf59
danieldinella <daniel.dinella@hotmail.com>
2024-10-10 11:28:00 +0200

    DOM

---
+++ DOM Challenge Starting Files/index.js
@@ -0,0 +1,4 @@
+document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Daniel";
+document.querySelector("li").style.color = "red";
+document.querySelector("button").style.backgroundColor = "yellow";
+document.querySelector(".btn").classList.toggle("huge")
